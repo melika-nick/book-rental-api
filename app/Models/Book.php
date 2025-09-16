aacfdb102e477622b9b89ae3b9258d699927a7c2
melika-nick <nikmelika8@gmail.com>
2025-09-16 11:09:46 +0330

    develop book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Book extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'author',
+        'isbn',
+        'stock',
+        'published_at',
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
+
+
+    public function rentals() : HasMany
+    {
+        return $this->hasMany(Rental::class);
+    }
 }
