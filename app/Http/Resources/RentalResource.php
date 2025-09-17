86f66eb028e4deb55cc9c31f1b99ab422a078ec8
melika-nick <nikmelika8@gmail.com>
2025-09-17 11:33:52 +0330

    develop rental resource

--- app/Http/Resources/RentalResource.php
+++ app/Http/Resources/RentalResource.php
@@ -14,6 +14,21 @@ class RentalResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id'          => $this->id,
+            'book'        => [
+                'id'     => $this->book->id,
+                'title'  => $this->book->title,
+                'author' => $this->book->author,
+            ],
+            'user'        => [
+                'id'   => $this->user->id,
+                'name' => $this->user->name,
+            ],
+            'rented_at'   => $this->rented_at?->toDateString(),
+            'due_at'      => $this->due_at?->toDateString(),
+            'returned_at' => $this->returned_at?->toDateString(),
+            'fine_amount' => $this->fine_amount,
+        ];
     }
 }
