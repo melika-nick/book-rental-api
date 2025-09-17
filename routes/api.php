e0188d47ab7654fc57c71022498bad5f2d5fc85b
melika-nick <nikmelika8@gmail.com>
2025-09-17 16:24:39 +0330

    add auth routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\BookController;
 use App\Http\Controllers\Admin\RentalController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,10 +19,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('books', BookController::class);
 
-Route::prefix('admin')->group(function () {
+//Route::prefix('admin')->group(function () {
+//    Route::apiResource('rentals', RentalController::class)->only([
+//        'index', 'store', 'show'
+//    ]);
+//    Route::post('rentals/{rental}/return', [RentalController::class, 'returnBook'])
+//        ->name('rentals.return');
+//});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// محافظت با Sanctum
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // مثال: متدهای رنتال فقط بعد از لاگین قابل دسترسی هستند
     Route::apiResource('rentals', RentalController::class)->only([
         'index', 'store', 'show'
     ]);
-    Route::post('rentals/{rental}/return', [RentalController::class, 'returnBook'])
-        ->name('rentals.return');
+    Route::post('/rentals/{id}/return', [RentalController::class, 'returnBook']);
 });
